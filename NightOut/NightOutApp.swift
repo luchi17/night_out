da202ddcfd48bbf5b8fda243af56bbdee5200859
mariluz <ml.parejo@usp.ceu.es>
2024-09-27 10:49:17 +0200

    Initial Commit

---
+++ NightOut/NightOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightOutApp.swift
+//  NightOut
+//
+//  Created by Apple on 27/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NightOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NightOutTests/NightOutTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightOutTests.swift
+//  NightOutTests
+//
+//  Created by Apple on 27/9/24.
+//
+
+import Testing
+@testable import NightOut
+
+struct NightOutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
