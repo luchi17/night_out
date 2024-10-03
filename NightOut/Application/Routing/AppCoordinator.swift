d891dc556f83aaeaa940599aad36233f6265458a
mariluz <luchi.parejo17@gmail.com>
2024-10-03 19:32:00 +0200

    Clean code

--- NightOut/Application/Routing/AppCoordinator.swift
+++ NightOut/Application/Routing/AppCoordinator.swift
@@ -2,40 +2,10 @@ import UIKit
 import SwiftUI
 import Combine
 
-
-// MARK: Set Flows
-private extension AppCoordinator {
-    func setMainFlow() {
-        let tabBarCoordinator = coordinatorFactory.makeTabBarCoordinator(path: path)
-        self.push(tabBarCoordinator)
-    }
-
-    
-    func showLogin() {
-        let loginCoordinator = LoginCoordinator()
-        self.push(loginCoordinator)
-    }
-
-    func navigateToOnboardingFlow() {
-//        let onboardingNVC = NavigationController()
-//        let router = HorizontalRouter(
-//            navigationController: onboardingNVC
-//        )
-//
-//        let onboardingCoordinator = coordinatorFactory.makeOnboardingCoordinator(
-//            router: router,
-//            onCompletion: setMainFlow
-//        )
-//        onboardingCoordinator.build()
-//        window.setRootViewController(onboardingNVC)
-    }
-}
-
+//https://github.com/TharinduKetipe/MVVMC-SwiftUI/blob/main/Coordinator/Settings/SettingsFlowCoordinator.swift
 
 final class AppCoordinator: ObservableObject {
     @Published var path: NavigationPath
-    private var cancellables = Set<AnyCancellable>()
-    
     
     var coordinatorFactory: CoordinatorFactoryImpl = {
         return CoordinatorFactoryImpl()
@@ -62,45 +32,23 @@ final class AppCoordinator: ObservableObject {
         let splashView = coord.build()
         return splashView
     }
-//
-//    // MARK: Flow Control Methods
-//    private func homeFlow() {
-//        let usersFlowCoordinator = HomeCoordinator()
-//        self.bind(userCoordinator: usersFlowCoordinator)
-//        self.push(usersFlowCoordinator)
-//    }
-//    
-//    private func searchFlow() {
-//        let settingsFlowCoordinator = SearchCoordinator()
-//        self.bind(settingsCoordinator: settingsFlowCoordinator)
-//        self.push(settingsFlowCoordinator)
-//    }
-//    
-//    private func publishFlow() {
-//        let profileFlowCoordinator = PublishCoordinator()
-//        self.bind(profileCoordinator: profileFlowCoordinator)
-//        self.push(profileFlowCoordinator)
-//    }
-//    
-//    private func mapFlow() {
-//        let profileFlowCoordinator = ProfileFlowCoordinator(page: .main)
-//        self.bind(profileCoordinator: profileFlowCoordinator)
-//        self.push(profileFlowCoordinator)
-//    }
-//    
-//    private func userFlow() {
-//        let profileFlowCoordinator = ProfileFlowCoordinator(page: .main)
-//        self.bind(profileCoordinator: profileFlowCoordinator)
-//        self.push(profileFlowCoordinator)
-//    }
+    
+    private func showTabView() {
+        let tabBarCoordinator = coordinatorFactory.makeTabBarCoordinator(path: path)
+        self.push(tabBarCoordinator)
+    }
+    
+    private func showLogin() {
+        let loginCoordinator = coordinatorFactory.makeLogin()
+        self.push(loginCoordinator)
+    }
     
 }
-
 
 private extension AppCoordinator {
     func makeSplashActions() -> SplashPresenterImpl.Actions {
         return .init(
-            onMainFlow: setMainFlow,
+            onMainFlow: showTabView,
             onLogin: showLogin
         )
     }
