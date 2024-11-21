2deae296d108798be0237c6b22871754090592e0
Mariluz <luchi.parejo17@gmail.com>
2024-11-21 19:58:11 +0100

    Fix user defaults

--- NightOut/Application/Extension/UserDefaultsExtension.swift
+++ NightOut/Application/Extension/UserDefaultsExtension.swift
@@ -2,22 +2,33 @@ import Foundation
 
 //Remove: not used
 private let userLoggedIn: String = "userLoggedIn"
-private let locationModels: String = "locationModels"
+private let companies: String = "locationModels"
+private let followModel: String = "followModel"
 
 extension UserDefaults {
     
     static func objectForKey(_ key: String) -> Any? {
-        return UserDefaults.standard.object(forKey: key)
+        // Obtener los datos almacenados
+            if let savedData = UserDefaults.standard.data(forKey: key) {
+                // Decodificar los datos a un FollowModel
+                if let decoded = try? JSONDecoder().decode(FollowModel.self, from: savedData) {
+                    return decoded
+                }
+            }
+            return nil
     }
 
     static func setObject(_ object: Any?, forKey key: String) {
         guard object != nil else {
-            if self.objectForKey(key) != nil { self.removeObjectForKey(key) }
-
+            if self.objectForKey(key) != nil {
+                self.removeObjectForKey(key)
+            }
             return
         }
-
-        UserDefaults.standard.set(object, forKey: key)
+        
+        if let encoded = try? JSONEncoder().encode(followModel) {
+            UserDefaults.standard.set(encoded, forKey: key)
+        }
     }
 
     static func removeObjectForKey(_ key: String) {
@@ -32,11 +43,19 @@ extension UserDefaults {
         self.objectForKey(userLoggedIn) as? Bool ?? false
     }
     
-    static func setCompanies(_ value: [LocationModel]) {
-        self.setObject(value, forKey: locationModels)
+    static func setCompanies(_ value: CompanyUsersModel) {
+        self.setObject(value, forKey: companies)
     }
 
-    static func getCompanies() -> [LocationModel] {
-        self.objectForKey(locationModels) as? [LocationModel] ?? []
+    static func getCompanies() -> CompanyUsersModel? {
+        self.objectForKey(companies) as? CompanyUsersModel
+    }
+    
+    static func setFollowModel(_ value: FollowModel) {
+        self.setObject(value, forKey: followModel)
+    }
+
+    static func getFollowModel() -> FollowModel? {
+        self.objectForKey(followModel) as? FollowModel
     }
 }
