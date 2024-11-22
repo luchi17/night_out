af429bd46e2a989f1e68697c2692810d96639c98
Mariluz <luchi.parejo17@gmail.com>
2024-11-22 11:49:53 +0100

    Fix user defaults not working

--- NightOut/Application/Extension/UserDefaultsExtension.swift
+++ NightOut/Application/Extension/UserDefaultsExtension.swift
@@ -5,30 +5,33 @@ private let followModel: String = "followModel"
 
 extension UserDefaults {
     
-    static func objectForKey(_ key: String) -> Any? {
-        // Obtener los datos almacenados
-            if let savedData = UserDefaults.standard.data(forKey: key) {
-                // Decodificar los datos a un FollowModel
-                if let decoded = try? JSONDecoder().decode(FollowModel.self, from: savedData) {
-                    return decoded
-                }
-            }
+    static func objectForKey<T: Codable>(forKey key: String, as type: T.Type) -> T? {
+        guard let data = UserDefaults.standard.data(forKey: key) else { return nil }
+        let decoder = JSONDecoder()
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            print("Error al decodificar el objeto: \(error.localizedDescription)")
             return nil
-    }
-
-    static func setObject(_ object: Any?, forKey key: String) {
-        guard object != nil else {
-            if self.objectForKey(key) != nil {
-                self.removeObjectForKey(key)
-            }
-            return
-        }
-        
-        if let encoded = try? JSONEncoder().encode(followModel) {
-            UserDefaults.standard.set(encoded, forKey: key)
         }
     }
-
+    
+    static func setObject<T: Codable>(_ object: T, forKey key: String) {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(object)
+            UserDefaults.standard.set(data, forKey: key)
+        } catch {
+            print("Error al codificar el objeto: \(error.localizedDescription)")
+        }
+    }
+    
+    static func clearAll() {
+        guard let appDomain = Bundle.main.bundleIdentifier else { return }
+        UserDefaults.standard.removePersistentDomain(forName: appDomain)
+        UserDefaults.standard.synchronize()
+    }
+    
     static func removeObjectForKey(_ key: String) {
         UserDefaults.standard.removeObject(forKey: key)
     }
@@ -36,16 +39,16 @@ extension UserDefaults {
     static func setCompanies(_ value: CompanyUsersModel) {
         self.setObject(value, forKey: companies)
     }
-
+    
     static func getCompanies() -> CompanyUsersModel? {
-        self.objectForKey(companies) as? CompanyUsersModel
+        self.objectForKey(forKey: companies, as: CompanyUsersModel.self)
     }
     
     static func setFollowModel(_ value: FollowModel) {
         self.setObject(value, forKey: followModel)
     }
-
+    
     static func getFollowModel() -> FollowModel? {
-        self.objectForKey(followModel) as? FollowModel
+        self.objectForKey(forKey: companies, as: FollowModel.self)
     }
 }
