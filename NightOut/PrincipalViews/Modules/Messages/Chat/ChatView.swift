ac9e97be2352e9c3e70b22141083d75beec02773
Mariluz <luchi.parejo17@gmail.com>
2025-02-05 14:54:50 +0100

    Chatview keyboard fix

--- NightOut/PrincipalViews/Modules/Messages/Chat/ChatView.swift
+++ NightOut/PrincipalViews/Modules/Messages/Chat/ChatView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 import Combine
+import Foundation
 
 struct ChatView: View {
     
     private let viewDidLoadPublisher = PassthroughSubject<Void, Never>()
     private let sendMessagePublisher = PassthroughSubject<Void, Never>()
     private let goBackPublisher = PassthroughSubject<Void, Never>()
+    
+    @ObservedObject private var keyboardObserver = KeyboardObserver()
     
     @ObservedObject var viewModel: ChatViewModel
     let presenter: ChatPresenter
@@ -24,26 +27,30 @@ struct ChatView: View {
                 ScrollView {
                     VStack(alignment: .leading) {
                         ForEach(viewModel.messages) { message in
-                            HStack {
-                                if message.sender == viewModel.myUid {
-                                    Spacer()
-                                    Text(message.message)
-                                        .padding(.all, 8)
-                                        .background(Color.blue)
-                                        .cornerRadius(10)
-                                        .foregroundColor(.white)
-                                } else {
-                                    Text(message.message)
-                                        .padding(.all, 8)
-                                        .background(Color.white)
-                                        .cornerRadius(10)
-                                        .foregroundColor(.black)
-                                    Spacer()
-                                }
-                            }
-                            .padding(.horizontal)
+                            MessageBubble(
+                                message: message,
+                                isFromCurrentUser: message.sender == viewModel.myUid
+                            )
+                            .id(message.id)
                         }
                     }
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, keyboardObserver.keyboardHeight + 50) // Ajusta el padding según el teclado
+                    .animation(.easeOut(duration: 0.2), value: keyboardObserver.keyboardHeight + 50) // Suaviza la animación del teclado
+                    .onChange(of: viewModel.messages.count) {
+                        withAnimation {
+                            proxy.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
+                        }
+                        
+                    }
+                }
+                .onChange(of: keyboardObserver.keyboardHeight) {
+                    // Cuando el teclado aparece o desaparece, desplazamos la vista
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        withAnimation {
+                            proxy.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
+                        }
+                   }
                 }
             }
             
@@ -67,6 +74,10 @@ struct ChatView: View {
         )
         .onAppear {
             viewDidLoadPublisher.send()
+        }
+        .onTapGesture {
+            // Cerrar el teclado cuando tocas fuera de él
+            hideKeyboard()
         }
     }
     
@@ -98,5 +109,57 @@ private extension ChatView {
             sendMessage: sendMessagePublisher.eraseToAnyPublisher()
         )
         presenter.transform(input: input)
+    }
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+
+class KeyboardObserver: ObservableObject {
+    @Published var keyboardHeight: CGFloat = 0
+
+    private var cancellables: Set<AnyCancellable> = []
+
+    init() {
+        // Observa la notificación de la aparición del teclado
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .map { notification -> CGFloat in
+                guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+                    return 0
+                }
+                return keyboardFrame.height
+            }
+            .sink { [weak self] height in
+                self?.keyboardHeight = height
+            }
+            .store(in: &cancellables)
+
+        // Observa la notificación de la desaparición del teclado
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+            .sink { [weak self] height in
+                self?.keyboardHeight = height
+            }
+            .store(in: &cancellables)
+    }
+}
+
+
+struct MessageBubble: View {
+    let message: MessageModel
+    let isFromCurrentUser: Bool
+    
+    var body: some View {
+        HStack {
+            if isFromCurrentUser { Spacer() }
+            Text(message.message)
+                .padding(.all, 8)
+                .background(isFromCurrentUser ? Color.blue : Color.white)
+                .foregroundColor(isFromCurrentUser ? Color.white : Color.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            if !isFromCurrentUser { Spacer() }
+        }
     }
 }
