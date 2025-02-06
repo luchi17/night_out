502e320f1da4d44816153470b7d246084534d48e
Mariluz <luchi.parejo17@gmail.com>
2025-02-06 12:44:19 +0100

    Fix camera view bottom buttons

--- NightOut/PrincipalViews/Modules/Publish/AddPostView.swift
+++ NightOut/PrincipalViews/Modules/Publish/AddPostView.swift
@@ -30,60 +30,19 @@ struct AddPostView: View {
     
     var body: some View {
         ZStack {
-            CameraPreview(session: cameraModel.session)
-                .ignoresSafeArea()
-                .opacity(viewModel.capturedImage == nil ? 1 : 0)
-            
-            if let image = viewModel.capturedImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
+            if viewModel.capturedImage == nil {
+                CameraPreview(session: cameraModel.session)
                     .ignoresSafeArea()
                 
-                VStack {
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            viewModel.capturedImage = nil
-                        }) {
-                            Image(systemName: "xmark.circle.fill")
-                                .font(.largeTitle)
-                                .foregroundColor(.white)
-                                .padding()
-                        }
-                    }
-                    Spacer()
-                }
-            }
-            
-            VStack {
-                Spacer()
-                HStack {
-                    Button(action: {
-                        cameraModel.switchCamera()
-                    }) {
-                        Image(systemName: "arrow.triangle.2.circlepath.camera")
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                            .padding()
-                    }
-                    .background(Color.black.opacity(0.5))
-                    .clipShape(Circle())
-                    .padding(.leading, 20)
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        cameraModel.capturePhoto { image in
-                            viewModel.capturedImage = image
-                        }
-                    }) {
-                        Circle()
-                            .stroke(Color.white, lineWidth: 4)
-                            .frame(width: 70, height: 70)
-                    }
-                    .padding(.bottom, 20)
-                }
+                notCapturedImageButtonsView
+                
+            } else if let image = viewModel.capturedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                
+                capturedImageButtonsView
             }
         }
         .onAppear {
@@ -91,6 +50,100 @@ struct AddPostView: View {
         }
         .onDisappear {
             cameraModel.stopSession()
+        }
+    }
+    
+    var capturedImageButtonsView: some View {
+        VStack {
+            Spacer()
+            
+            ZStack(alignment: .bottomLeading) {
+                HStack {
+                    Spacer()
+                    
+                    sendPhotoView
+                    
+                    Spacer()
+                }
+                
+                cancelPhotoView
+                    .padding(.leading, 20)
+            }
+        }
+        .padding(.bottom, 30)
+    }
+    
+    var notCapturedImageButtonsView: some View {
+        VStack {
+            Spacer()
+            
+            ZStack(alignment: .bottomLeading) {
+                HStack {
+                    Spacer()
+                    
+                    capturePhotoView
+                    
+                    Spacer()
+                }
+                
+                switchCameraView
+                    .padding(.leading, 20)
+            }
+        }
+        .padding(.bottom, 30)
+    }
+    
+    var switchCameraView: some View {
+        Button(action: {
+            cameraModel.switchCamera()
+        }) {
+            Image(systemName: "arrow.triangle.2.circlepath.camera")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 35, height: 35)
+                .foregroundColor(.white)
+                .padding(.all, 8)
+        }
+        .background(Color.black.opacity(0.5))
+        .clipShape(Circle())
+        .padding(.leading, 20)
+    }
+    
+    var capturePhotoView: some View {
+        Button(action: {
+            cameraModel.capturePhoto { image in
+                viewModel.capturedImage = image
+            }
+        }) {
+            Circle()
+                .stroke(Color.white, lineWidth: 4)
+                .frame(width: 50, height: 50)
+        }
+    }
+    
+    var cancelPhotoView: some View {
+        Button(action: {
+            viewModel.capturedImage = nil
+        }) {
+            Image(systemName: "xmark.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.white)
+                .padding()
+        }
+    }
+    
+    var sendPhotoView: some View {
+        Button(action: {
+           // uploadPostPublisher.send()
+        }) {
+            Image(systemName: "paperplane.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.white)
+                .padding()
         }
     }
 }
@@ -129,3 +182,33 @@ private extension AddPostView {
 //.sheet(isPresented: $showingLocationPicker) {
 ////            LocationPicker(selectedLocation: $viewModel.location)
 //}
+
+
+//flash:
+//
+//class CameraViewModel: NSObject, ObservableObject {
+//    let session = AVCaptureSession()
+//    private var photoOutput = AVCapturePhotoOutput()
+//    private var currentDevice: AVCaptureDevice?
+//    private var isUsingFrontCamera = false
+//    private var captureCompletion: ((UIImage?) -> Void)?
+//    
+//    @Published var isFlashOn = false // 🔦 Estado del flash
+//
+//    override init() {
+//        super.init()
+//        configureCamera()
+//    }
+//
+//    func toggleFlash() {
+//        isFlashOn.toggle()
+//    }
+//
+//    func capturePhoto(completion: @escaping (UIImage?) -> Void) {
+//        let settings = AVCapturePhotoSettings()
+//        settings.flashMode = isFlashOn ? .on : .off
+//        captureCompletion = completion
+//        photoOutput.capturePhoto(with: settings, delegate: self)
+//    }
+//}
+//
