4f8ca83207e281d3d69e3e80ab894dc237490e86
Mariluz <luchi.parejo17@gmail.com>
2025-03-03 17:59:59 +0000

    Fix splash black screen

--- NightOut/PrincipalViews/Modules/Splash/SplashView.swift
+++ NightOut/PrincipalViews/Modules/Splash/SplashView.swift
@@ -48,18 +48,29 @@ struct SplashView: View {
                 VStack {
                     Spacer()
                     
-                    if viewModel.isReady {
-                        VideoPlayer(player: viewModel.player)
-                            .overlay(Rectangle().fill(Color.clear))
-                            .allowsHitTesting(false) // Deshabilita la interacción
-                            .frame(width: 300, height: 300, alignment: .center)
-                            .padding(.horizontal)
-                    } else {
-                        Image("logo_amarillo")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 150, height: 150)
-                    }
+                    VideoPlayer(player: viewModel.player)
+                        .allowsHitTesting(false) // Deshabilita la interacción
+                        .frame(width: 300, height: 300, alignment: .center)
+                        .padding(.horizontal)
+                        .overlay {
+                            Group {
+                                if viewModel.isReady {
+                                    Rectangle().fill(Color.clear)
+                                } else {
+                                    
+                                    ZStack {
+                                        Color.darkBlueColor
+                                            .ignoresSafeArea()
+                                        
+                                        Image("logo_amarillo")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 150, height: 150)
+                                    }
+                                    
+                                }
+                            }
+                        }
                     
                     Spacer()
                 }
@@ -92,6 +103,7 @@ class VideoPlayerViewModel: ObservableObject {
     var player: AVPlayer?
     private var playerItem: AVPlayerItem?
     private var playerObserver: AnyCancellable?
+    private var readyCheckCancellable: AnyCancellable?
     
     @Published var isReady: Bool = false
     @Published var isFinished: Bool = false
@@ -107,9 +119,24 @@ class VideoPlayerViewModel: ObservableObject {
         playerObserver = playerItem?.publisher(for: \.status)
             .sink { [weak self] status in
                 if status == .readyToPlay {
+
                     self?.player?.play()
-                    self?.isReady = true
-                    print("ready")
+
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                        self?.isReady = true
+                    }
+                    
+                }
+            }
+        
+        
+        // Iniciar el temporizador para verificar el estado después de 2 segundos
+        readyCheckCancellable = Just(())
+            .delay(for: .seconds(2), scheduler: DispatchQueue.main)
+            .sink { [weak self] in
+                if self?.playerItem?.status != .readyToPlay {
+                    print("Error: El video no está listo para reproducirse después de 2 segundos.")
+                    self?.isFinished = true
                 }
             }
         
