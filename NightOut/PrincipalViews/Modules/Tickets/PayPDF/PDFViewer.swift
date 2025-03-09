7bbdef67f0e03dfdeba530009d6016d8507cac31
Mariluz <luchi.parejo17@gmail.com>
2025-03-09 22:50:36 +0100

    Clean code

--- NightOut/PrincipalViews/Modules/Tickets/PayPDF/PDFViewer.swift
+++ NightOut/PrincipalViews/Modules/Tickets/PayPDF/PDFViewer.swift
@@ -2,37 +2,6 @@ import SwiftUI
 import UIKit
 import PDFKit
 
-//// Representable para abrir PDF
-//struct PDFViewer: UIViewControllerRepresentable {
-//    
-//    var pdfURL: URL
-//
-//    func makeUIViewController(context: Context) -> UIDocumentInteractionController {
-//        let controller = UIDocumentInteractionController(url: pdfURL)
-//        controller.delegate = context.coordinator
-//        return controller
-//    }
-//
-//    func updateUIViewController(_ uiViewController: UIDocumentInteractionController, context: Context) {}
-//
-//    // Coordinator para manejar los eventos de presentación
-//    class Coordinator: NSObject, UIDocumentInteractionControllerDelegate {
-//        
-//        func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
-//            return UIApplication.shared.windows.first!.rootViewController!
-//        }
-//
-//        func documentInteractionControllerDidEndPreview(_ controller: UIDocumentInteractionController) {
-//            print("Preview ended")
-//        }
-//    }
-//
-//    func makeCoordinator() -> Coordinator {
-//        return Coordinator()
-//    }
-//}
-
-
 struct PDFKitView: UIViewRepresentable {
     let url: URL // new variable to get the URL of the document
     
