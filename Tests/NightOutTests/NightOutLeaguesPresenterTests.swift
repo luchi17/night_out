db7331d36c94adf0f44c41b142f1e5bf91ff071d
Mariluz <luchi.parejo17@gmail.com>
2025-06-16 11:49:58 +0200

    Add new LeaguePresenter test failing

--- Tests/NightOutTests/NightOutLeaguesPresenterTests.swift
+++ Tests/NightOutTests/NightOutLeaguesPresenterTests.swift
@@ -11,7 +11,7 @@ import Combine
 
 final class MockLeaguesUserDataUseCase: UserDataUseCase {
     var userInfoSubject = PassthroughSubject<UserModel?, Never>()
-
+    
     func getUserInfo(uid: String) -> AnyPublisher<UserModel?, Never> {
         return userInfoSubject.eraseToAnyPublisher()
     }
@@ -34,7 +34,7 @@ final class LeaguePresenterTests: XCTestCase {
     private var sut: LeaguePresenterImpl!
     private var mockUserDataUseCase: MockLeaguesUserDataUseCase!
     private var cancellables: Set<AnyCancellable> = []
-
+    
     override func setUp() {
         super.setUp()
         
@@ -48,10 +48,11 @@ final class LeaguePresenterTests: XCTestCase {
             goToCreateLeague: {},
             goToLeagueDetail: { _ in }
         )
+
         
         sut = LeaguePresenterImpl(useCases: useCases, actions: actions)
     }
-
+    
     func test_viewDidLoad_showsNoLeaguesDialog_whenUserHasNoLeagues() {
         // Given
         let viewDidLoadSubject = PassthroughSubject<Void, Never>()
@@ -61,23 +62,56 @@ final class LeaguePresenterTests: XCTestCase {
             openCreateLeague: Empty().eraseToAnyPublisher(),
             openLeagueDetail: Empty().eraseToAnyPublisher()
         )
-
+        
         sut.transform(input: input)
-
+        
         // When
         let user = UserModel(uid: "", fullname: "", username: "", email: "", misLigas: [:])
         viewDidLoadSubject.send(())
         mockUserDataUseCase.userInfoSubject.send(user)
-
+        
         // Then
         let expectation = XCTestExpectation(description: "Espera cambio en el ViewModel")
-
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             XCTAssertFalse(self.sut.viewModel.loading)
             XCTAssertTrue(self.sut.viewModel.showNoLeaguesDialog)
             expectation.fulfill()
         }
-
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func test_deleteLeague_removesLeagueFromViewModel() {
+        // Given
+        let testLeague = League(
+            leagueId: "123",
+            name: "Test League",
+            drinks: 0,
+            imageName: "copa1"
+        )
+        sut.viewModel.leaguesList = [testLeague]
+        
+        let deleteLeagueSubject = PassthroughSubject<League, Never>()
+        let input = LeaguePresenterImpl.ViewInputs(
+            viewDidLoad: Empty().eraseToAnyPublisher(),
+            deleteLeague: deleteLeagueSubject.eraseToAnyPublisher(),
+            openCreateLeague: Empty().eraseToAnyPublisher(),
+            openLeagueDetail: Empty().eraseToAnyPublisher()
+        )
+        
+        sut.transform(input: input)
+        
+        // When
+        deleteLeagueSubject.send(testLeague)
+        
+        // Then
+        let expectation = XCTestExpectation(description: "Esperamos que la liga se elimine")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertTrue(self.sut.viewModel.leaguesList.isEmpty)
+            expectation.fulfill()
+        }
+        
         wait(for: [expectation], timeout: 1.0)
     }
 }
