555eadda11b8e86c2a9d5874f736ea796db92330
Mariluz <luchi.parejo17@gmail.com>
2025-06-24 19:08:57 +0200

    Show signup company register

--- NightOut/Modules/Init/Login/LoginView.swift
+++ NightOut/Modules/Init/Login/LoginView.swift
@@ -136,6 +136,20 @@ struct LoginView: View, Hashable {
         .background(
             Color.blackColor
         )
+        .alert(isPresented: $showRegisterAlert) {
+            Alert(
+                title: Text("Selecciona una opción"),
+                message: Text("¿Cómo quieres registrarte?"),
+                primaryButton: .default(Text("Registrar Empresa"), action: {
+                    showRegisterAlert = false
+                    signupCompanyPublisher.send()
+                }),
+                secondaryButton: .default(Text("Registrar Persona"), action: {
+                    showRegisterAlert = false
+                    signupUserPublisher.send()
+                })
+            )
+        }
         .showToast(
             error: (
                 type: viewModel.toast,
@@ -185,7 +199,7 @@ struct LoginView: View, Hashable {
     
     private var signupButton: some View {
         Button(action: {
-            signupUserPublisher.send()
+            showRegisterAlert = true
         }) {
             Text("¿Cuenta nueva? Regístrate".uppercased())
                 .font(.system(size: 17, weight: .bold))
